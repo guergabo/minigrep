75f33b16f67c05e87d5626a05aae978ca8bc4af5
Gabriel Guerra <guergabo@umich.edu>
2022-01-04 07:27:52 -0500

    minigrep mvp

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-/* minigrep */
-
+/* minigrep: leave argument collecting and error handling
+in src/main.rs */
 use std::env; 
 use std::process; 
 
 use minigrep::Config; 
 
+// CASE_INSENSITIVE=1 cargo run to poem.txt
 fn main() {
     let args: Vec<String> = env::args().collect(); 
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err); 
+        eprintln!("Problem parsing arguments: {}", err); 
         process::exit(1);
     }); 
 
-    println!("Searching for {}", config.query); 
-    println!("In file {}", config.filename); 
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e); 
-
+        eprintln!("Application error: {}", e); 
         process::exit(1); 
     }
 }
